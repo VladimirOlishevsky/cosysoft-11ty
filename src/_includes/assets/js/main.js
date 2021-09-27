da73ec23006ba37cd59ee7c2a651d54a008c1115
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2021-09-27 21:36:30 +0400

    catch error when page don't used js

--- src/_includes/assets/js/main.js
+++ src/_includes/assets/js/main.js
@@ -1,11 +1,17 @@
 import { annotate } from 'rough-notation';
 
 const engineer = document.querySelector("#frontend-engineer");
-const annotation = annotate(engineer, {
-    type: "highlight",
-    color: "#A2E8FA",
-    multiline: true,
-});
-annotation.show();
 
+if (engineer) {
+    const annotation = annotate(engineer, {
+        type: "highlight",
+        color: "#A2E8FA",
+        multiline: true,
+    });
+    annotation.show();
+}
+
+
+console.log('hello')
+console.log('hello')
 console.log('hello')
