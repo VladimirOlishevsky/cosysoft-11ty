044f5282490e7ae3319d060b93a2eceffdd5f8b7
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2021-11-11 20:06:49 +0400

    add readme.md, buils js by esbuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { build } = require("esbuild");
+const production = process.env.NODE_ENV === `production` // true when NODE_ENV is production
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { toLowerCase, limit } = require('./src/_includes/assets/js/filter')
 
@@ -9,6 +11,17 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('makeLowerCase', toLowerCase);
   eleventyConfig.addFilter('limit', limit);
   eleventyConfig.addShortcode('youtube', require('./src/_includes/assets/js/youtube'));
+
+  eleventyConfig.addWatchTarget("./src/_includes/assets/js");
+  eleventyConfig.addWatchTarget('./src/filters');
+  eleventyConfig.on("beforeBuild", () => {
+    build({
+      entryPoints: ["./src/_includes/assets/js/main.js"],
+      outfile: "dist/scripts/main.js",
+      bundle: true,
+      minify: production,
+    }).catch(() => process.exit(1));
+  });
 
   return {
     templateFormats: ["md", "njk", "png", "jpg", "jpeg"],
---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">Cosysoft 11ty</h1>
+
+<img src="./assets/cosysoft-11ty.gif" width="100%">
+
+## Description
+
+This small app builded on [11ty](https://www.11ty.dev/). You can see the most popular things during develop you personal site or blog, for example:
+
+- using Markdown(.md) and NJK(.njk) templates
+- pages routing
+- using 11ty filters and shortcodes
+- add youtube video to your site
+- async operations and caching data
+- pagination
+- img static files
+- build javascript code with esbuild
+
+## Where to begin:
+- Clone the repository https://github.com/VladimirOlishevsky/atm-test
+- Set ```yarn``` dependencies
+- Start a ```yarn start``` project.
+- Enjoy it :)
+
